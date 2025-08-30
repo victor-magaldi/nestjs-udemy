e13e8f0eef2e30b6765672282c33a570b31a52ed
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2025-08-29 22:52:06 -0300

    refactor: refactor code

--- messages/src/messages/messages.repository.ts
+++ messages/src/messages/messages.repository.ts
@@ -1,32 +1,47 @@
-import { throws } from "node:assert";
 import { readFile, writeFile } from "node:fs/promises";
+export interface Message {
+  id: string | number;
+  content: string;
+}
+
+export interface MessagesMap {
+  [id: string]: Message;
+}
 
 export class MessagesRepository {
+  private db: MessagesMap;
+  constructor() {
+    this.getContent()
+  }
   async findOne(id: string) {
-    const messages = await this.getContent()
-
-    return messages[id]
+    await this.getContent()
+    return this.db[id]
   }
   async findAll() {
-    const messages = await this.getContent()
-
-    return messages
+    await this.getContent()
+    return this.db
   }
   async create(message: string) {
     const messages = await this.getContent()
-
     const id = Math.floor(Math.random() * 999)
 
-    messages[id] = {
+    const messagesObj = Object.assign({}, messages)
+
+    messagesObj[id] = {
       id,
       content: message
     }
-    await writeFile('messages.json', JSON.stringify(messages), "utf-8")
+    this.persist(messagesObj)
   }
 
   private async getContent() {
     const contents = await readFile('messages.json', 'utf8')
-    const messages = JSON.parse(contents)
+    const messages: MessagesMap = JSON.parse(contents)
+    this.db = messages
     return messages
+  }
+  private async persist(messages: MessagesMap) {
+    await writeFile('messages.json', JSON.stringify(messages), "utf-8")
+    this.db = messages
   }
 }
