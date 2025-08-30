5007979e2d04655090bf4773450456478e3a4014
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2025-08-29 22:09:42 -0300

    feat: implementing other methods repository

--- messages/src/messages/messages.repository.ts
+++ messages/src/messages/messages.repository.ts
@@ -2,14 +2,27 @@ import { readFile, writeFile } from "node:fs/promises";
 
 export class MesagesRepository {
   async findOne(id: string) {
-    const contents = await readFile('message.json', 'utf8')
-    const message = JSON.parse(contents)
-    return message[id]
+    const contents = await readFile('messages.json', 'utf8')
+    const messages = JSON.parse(contents)
+
+    return messages[id]
   }
   async findAll() {
+    const contents = await readFile('messages.json', 'utf8')
+    const messages = JSON.parse(contents)
 
+    return messages
   }
   async create(message: string) {
-    readFile
+    const contents = await readFile('messages.json', 'utf8')
+    const messages = JSON.parse(contents)
+
+    const id = Math.floor(Math.random() * 999)
+
+    messages[id] = {
+      id,
+      content: message
+    }
+    await writeFile('messages.json', 'utf8')
   }
 }
