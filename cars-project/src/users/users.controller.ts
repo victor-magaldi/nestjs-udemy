86f7598adac6c831479c3bc798b3e41b7089fbc3
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2025-10-03 21:18:59 -0300

    refactor: refactoring controller find user

--- cars-project/src/users/users.controller.ts
+++ cars-project/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Patch, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user-dto';
 import { UsersService } from './users.service';
 import { Response } from 'express';
@@ -13,16 +13,7 @@ export class UsersController {
   }
 
   @Get(`/:id`)
-  async getUserById(@Param('id') id: string, @Res() res: Response) {
-    const user = await this.usersService.findOne(Number(id))
-    if (!user) {
-      return res.status(404).json({
-        message: "User not found",
-        statusCode: 404
-      })
-    }
-    console.log(user)
-
-    return res.status(200).json(user)
+  async findUser(@Param('id') id: string) {
+    return this.usersService.findOne(Number(id))
   }
 }
