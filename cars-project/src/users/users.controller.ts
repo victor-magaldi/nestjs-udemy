d28e46d68f2d3a4ecda1e6bccf011a7064f8dbaa
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2025-10-03 21:51:17 -0300

    feat: add remove controller

--- cars-project/src/users/users.controller.ts
+++ cars-project/src/users/users.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Get, Param, Post, Patch, Query, Delete } from '@nestj
 import { CreateUserDto } from './dtos/create-user-dto';
 import { UsersService } from './users.service';
 import { Response } from 'express';
+import path from 'path';
+import { UpdateUserDto } from './dtos/update-user-dto';
 
 @Controller('auth')
 export class UsersController {
@@ -25,5 +27,10 @@ export class UsersController {
   @Delete('/:id')
   removeUsers(@Param('id') id: string) {
     return this.usersService.remove(Number(id))
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(Number(id), body)
   }
 }
