8d61956ab15ff3ba9d7edac7780edf9b2873b67c
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2025-10-05 19:16:18 -0300

    fead: add constructor in interceptor for select dto

--- cars-project/src/interceptors/serialize.interceptor.ts
+++ cars-project/src/interceptors/serialize.interceptor.ts
@@ -1,15 +1,15 @@
 import { UseInterceptors, NestInterceptor, ExecutionContext, CallHandler } from "@nestjs/common";
-import { plainToClass } from "class-transformer";
+import { plainToClass, plainToInstance } from "class-transformer";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
-import { UserDto } from "src/users/dtos/user-dto";
 
 export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) { }
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     // console.log('running before the handler', context)
 
     return handler.handle().pipe(map((data) => {
-      return plainToClass(UserDto, data, {
+      return plainToInstance(this.dto, data, {
         excludeExtraneousValues: true
       })
     }))
