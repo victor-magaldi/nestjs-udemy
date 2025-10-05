eb19372066bbec3eba2c51c451139713546c205a
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2025-10-05 19:07:20 -0300

    feat: add serialize in custom interceptor

--- cars-project/src/interceptors/serialize.interceptor.ts
+++ cars-project/src/interceptors/serialize.interceptor.ts
@@ -2,13 +2,16 @@ import { UseInterceptors, NestInterceptor, ExecutionContext, CallHandler } from 
 import { plainToClass } from "class-transformer";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
+import { UserDto } from "src/users/dtos/user-dto";
 
 export class SerializeInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
-    console.log('running before the handler', context)
+    // console.log('running before the handler', context)
 
     return handler.handle().pipe(map((data) => {
-      console.log('running before response is sent out', data)
+      return plainToClass(UserDto, data, {
+        excludeExtraneousValues: true
+      })
     }))
   }
 
